b222f07bf09f2969488273d2399f735dccf4326a
hugosmoreira <hugo.n4@gmail.com>
2022-02-27 19:06:31 -0800

    hotels in dashboard bug fix

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -9,6 +9,8 @@ export const create = async (req, res) => {
     let files = req.files;
 
     let hotel = new Hotel(fields);
+    hotel.postedBy = req.user._id;
+
     // handle image
     if (files.image) {
       hotel.image.data = fs.readFileSync(files.image.path);
@@ -40,12 +42,20 @@ export const hotels = async (req, res) => {
   res.json(all);
 };
 
-
 export const image = async (req, res) => {
-    let hotel = await Hotel.findById(req.params.hotelId).exec();
-    if (hotel && hotel.image && hotel.image.data !== null) {
-      res.set("Content-Type", hotel.image.contentType);
-      return res.send(hotel.image.data);
-    }
+  let hotel = await Hotel.findById(req.params.hotelId).exec();
+  if (hotel && hotel.image && hotel.image.data !== null) {
+    res.set("Content-Type", hotel.image.contentType);
+    return res.send(hotel.image.data);
+  }
+};
+
+
+export const sellerHotels = async (req, res) => {
+    let all = await Hotel.find({ postedBy: req.user._id })
+      .select("-image.data")
+      .populate("postedBy", "_id name")
+      .exec();
+     console.log(all);
+    res.send(all);
   };
-  
