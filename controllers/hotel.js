00e3cefa7dc6fcf323cad4971c4cb6b07715c48e
hugosmoreira <hugo.n4@gmail.com>
2022-02-28 18:35:11 -0800

    hotel update

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -10,7 +10,6 @@ export const create = async (req, res) => {
 
     let hotel = new Hotel(fields);
     hotel.postedBy = req.user._id;
-
     // handle image
     if (files.image) {
       hotel.image.data = fs.readFileSync(files.image.path);
@@ -50,12 +49,27 @@ export const image = async (req, res) => {
   }
 };
 
-
 export const sellerHotels = async (req, res) => {
-    let all = await Hotel.find({ postedBy: req.user._id })
-      .select("-image.data")
-      .populate("postedBy", "_id name")
-      .exec();
-     console.log(all);
-    res.send(all);
-  };
+  let all = await Hotel.find({ postedBy: req.user._id })
+    .select("-image.data")
+    .populate("postedBy", "_id name")
+    .exec();
+  // console.log(all);
+  res.send(all);
+};
+
+export const remove = async (req, res) => {
+  let removed = await Hotel.findByIdAndDelete(req.params.hotelId)
+    .select("-image.data")
+    .exec();
+  res.json(removed);
+};
+
+export const read = async (req, res) => {
+  let hotel = await Hotel.findById(req.params.hotelId)
+    .select("-image.data")
+    .exec();
+  console.log("SINGLE HOTEL", hotel);
+  res.json(hotel);
+};
+
